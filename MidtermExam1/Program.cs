4939d9dfbe2e515a751b1ad7fe8b263570dd5d09
antonynapony <tonypoltorabatko@gmail.com>
2023-11-13 22:04:38 +0300

    Add Code to Task1

--- MidtermExam1/Program.cs
+++ MidtermExam1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class MyStack
+{
+    public static void Main()
+    {
+        Stack<int> numbers = new Stack<int>(5);
+        numbers.Push(1);
+        numbers.Push(2);
+        numbers.Push(3);
+        numbers.Push(4);
+        numbers.Push(5);
+        int number1 = numbers.Pop();
+        Console.WriteLine(number1);
+        int number2 = numbers.Pop();
+        Console.WriteLine(number2);
+        int number3 = numbers.Pop();
+        Console.WriteLine(number3);
+        int number4 = numbers.Pop();
+        Console.WriteLine(number4);
+        int number5 = numbers.Pop();
+        Console.WriteLine(number5);
+    }
+}
