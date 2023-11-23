238957fb0d4d414e1ba1e7b4f6f6e27906104eca
antonynapony <tonypoltorabatko@gmail.com>
2023-11-23 22:53:25 +0300

    Add Code

---
+++ MidtermExam1/MyStack.cs
@@ -0,0 +1,35 @@
+﻿namespace MidtermExam1
+{
+    public class MyStack
+    {
+        private int[] _stack;
+        private int _pointer;
+
+        public MyStack(int size)
+        {
+            _stack = size > 0 ? new int[size] : throw new ArgumentException(nameof(size));
+            _pointer = -1;
+        }
+
+        public bool Push(int value)
+        {
+            bool isSuccess = true;
+            if (_pointer == _stack.Length - 1)
+            {
+                isSuccess = false;
+            }
+            _stack[++_pointer] = value;
+
+            return isSuccess;
+        }
+
+        public int? Pop()
+        {
+            if ( _pointer == -1)
+            {
+                return null;
+            }
+            return _stack[_pointer--];
+        }
+    }
+}
--- MidtermExam1/Program.cs
+++ MidtermExam1/Program.cs
@@ -1,22 +1,11 @@
-﻿class MyStack
-{
-    public static void Main()
-    {
-        Stack<int> numbers = new Stack<int>(5);
-        numbers.Push(1);
-        numbers.Push(2);
-        numbers.Push(3);
-        numbers.Push(4);
-        numbers.Push(5);
-        int number1 = numbers.Pop();
-        Console.WriteLine(number1);
-        int number2 = numbers.Pop();
-        Console.WriteLine(number2);
-        int number3 = numbers.Pop();
-        Console.WriteLine(number3);
-        int number4 = numbers.Pop();
-        Console.WriteLine(number4);
-        int number5 = numbers.Pop();
-        Console.WriteLine(number5);
-    }
-}
+﻿using MidtermExam1;
+
+MyStack stack = new MyStack(6);
+stack.Push(24);
+stack.Push(-22);
+stack.Push(12);
+stack.Push(98);
+stack.Push(7);
+stack.Push(9);
+var number = stack.Pop();
+Console.WriteLine(number);
